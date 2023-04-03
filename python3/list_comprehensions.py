f7c848d22656b7cb510c988e162c84a21960a5bb
whoisorioki <adrianorioki@gmail.com>
2023-04-03 15:07:31 +0300

    First commit(README.md, python3)

---
+++ python3/list_comprehensions.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    a = []
+    for i in range(x):
+        a.extend(i)
+    
+    print(a)
