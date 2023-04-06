950b32a0ba7830bad217c138f9601056b65bf8dc
whoisorioki <adrianorioki@gmail.com>
2023-04-06 11:34:31 +0300

    Find the percentage!

---
+++ python3/percentage.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    for key, value in student_marks.items():
+        if key == query_name:
+            average = sum(value) / 3
+
+    print(f"{average:.2f}")
