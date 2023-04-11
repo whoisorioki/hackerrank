75720f459edc4fa9237e49e090362f010c81df53
whoisorioki <adrianorioki@gmail.com>
2023-04-11 11:54:27 +0300

    Day 8 update

--- 30DaysOfCode/day-8.py
+++ 30DaysOfCode/day-8.py
@@ -2,14 +2,16 @@ if __name__ == '__main__':
     people = {}
     N = int(input())
     for _ in range(N):
-        name, *line = input().split()
-        number = list(map(int, line))
-        number = number[0]
+        name, number = input().split()
+        number = int(number)
         people[name] = number
 
-    for k, v in people.items(): 
-        person = input()
-        if person == k:
-            print(f'{k}={v}')
+    while True: 
+        try:
+            person = input()
+        except EOFError:
+            break
+        if person in people:
+            print(f"{person}={people[person]}")
         else:
-            print('Not found')
+            print("Not found")
