bd957e2689eb1b0fbd005a5d3fb684f7d26282bc
whoisorioki <adrianorioki@gmail.com>
2023-04-12 12:38:16 +0300

    Text Wrap

---
+++ python3/text_wrap.py
@@ -0,0 +1,16 @@
+import textwrap
+from textwrap import TextWrapper
+
+
+def wrap(string, max_width):
+    string = textwrap.wrap(text=string, width=max_width)
+    new_string = ''
+    for item in string:
+        new_string += item + '\n'
+    return new_string
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
