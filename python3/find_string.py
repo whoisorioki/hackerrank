c96e63dd38202fbec1d874a3a244e62b15b071d0
whoisorioki <adrianorioki@gmail.com>
2023-04-12 10:55:39 +0300

    Find String!

---
+++ python3/find_string.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+    count = 0
+    sub_len = len(sub_string)
+    for i in range(0, len(string)):
+        print(string[i:i+3])
+        if string[i:i+sub_len] == sub_string:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
